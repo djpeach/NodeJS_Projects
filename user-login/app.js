01d10b50ae26e65edec146d04156e86b1df4cef2
Daniel Peach <dpeaches96@gmail.com>
2018-03-07 22:36:13 -0500

    added middleware

--- user-login/app.js
+++ user-login/app.js
@@ -31,8 +31,45 @@ app.use(multer({dest: './uploads'}))
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Handle express-session middleware
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: 'true',
+  resave: true
+}));
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session()); // Needs to come after express-session
+
+// Validation (Legacy express-validator)
+app.use(ExpressValidator({
+  errorFormatter: (param, msg, value) => {
+    var namespace = param.split('.')
+    , root = namespace.shift()
+    , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param: formParam,
+      msg: msg,
+      value: value
+    }
+  }
+}));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Flash and express-messages
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 app.use('/', index);
 app.use('/users', users);
