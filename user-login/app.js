22c8bb6432dba6b57db0f166a99ab6ee63f6ae04
Daniel Peach <dpeaches96@gmail.com>
2018-03-07 22:21:36 -0500

    required modules

--- user-login/app.js
+++ user-login/app.js
@@ -3,7 +3,16 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
+var localStrategy = require('passport-local').Strategy;
 var bodyParser = require('body-parser');
+var multer = require('multer'); // Helps with file uploads
+var flash = require('connect-flash');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+
+var db = mongoose.connection;
 
 var index = require('./routes/index');
 var users = require('./routes/users');
